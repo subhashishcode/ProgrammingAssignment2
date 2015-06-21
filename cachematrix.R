cbcb504287832c87ee14cf84039eeb39dfa435e8
Subhashish Dutta <subhashish.dutta@altisource.com>
2015-06-21 22:24:54 +0530

    Programming assignment related changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions between themselves optimize matrix inverse calculation.
+## makeCacheMatrix caches the inverse of a matrix that is passed to it. 
+## The cached inverse is stored in a scope that makes it accessible by clients
+## of the makeCacheMatrix function.
+## The cacheSolve function returns the inverse of the matrix that is passed
+## to it. Instead of calculating the inverse always, it first uses the
+## makeCacheMatrix function to check if the inverse of the concerned matrix
+## is cached and if yes, returns that. If not, it calculates the inverse
+## and caches it using makeCacheMatrix
 
-## Write a short comment describing this function
+## This function caches the inverse of the matrix passed to it
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y)  {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function()   {
+        return(x)
+    }
+    setinverse <- function(inverse) {
+        inv <<- inverse
+    }
+    getinverse <- function()    {
+        return(inv)
+    }
+    list(set = set, get = get, 
+         setinverse = setinverse, 
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of the matrix passed to it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    funcs <- makeCacheMatrix(x,...)
+    inv <- funcs$getinverse()
+    if(!is.null(inv))   {
+        print("returning cached data")
+        return(inv)
+    }
+    matrix <- funcs$get()
+    inv <- solve(matrix,...)
+    funcs$setinverse(inv)
+    return(inv)
 }
